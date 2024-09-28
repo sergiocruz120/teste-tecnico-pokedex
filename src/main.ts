a89c55760bd7324399478d47bae65c63d52fbbd8
sergiocruz120 <122457897+sergiocruz120@users.noreply.github.com>
2024-09-28 18:52:24 -0300

    chore: install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+
 import './style.css'
 import App from './App.vue'
 import router from './router'
@@ -6,5 +8,9 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+const app = createApp(App)
+const pinia = createPinia();
 
-createApp(App).use(router).mount('#app')
+app.use(pinia)
+app.use(router)
+app.mount('#app')
