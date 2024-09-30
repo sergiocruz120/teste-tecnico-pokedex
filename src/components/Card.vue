e124f0fc15072b56977a394529c7dfa16031d447
sergiocruz120 <122457897+sergiocruz120@users.noreply.github.com>
2024-09-30 13:00:30 -0300

    Refactor(card): remuve unused code

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,12 +8,7 @@
 
 <style scoped>
 .container {
-  display: flex;
-  flex-wrap: wrap;
-  color: #323232;
   padding-bottom: 14px;
-  align-items: center;
-  justify-content: center;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
 }
