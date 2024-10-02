aa18ec54cb6866c8307b45b9b41eb57a57b95d3a
sergiocruz120 <122457897+sergiocruz120@users.noreply.github.com>
2024-10-02 14:17:05 -0300

    chore: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+    ],
+  },
 })
